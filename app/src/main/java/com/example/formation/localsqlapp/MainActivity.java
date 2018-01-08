b395f654904fbdc85404d0165577ead6722acc79
Formation <toto.titi@free.fr>
2018-01-08 12:20:26 +0100

    Ajout activité AjoutContact

--- app/src/main/java/com/example/formation/localsqlapp/MainActivity.java
+++ app/src/main/java/com/example/formation/localsqlapp/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.formation.localsqlapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * Lancement de l'activité formulaore au click sur un bouton
+     * appelé par la classe MainActivity
+     * @param view
+     */
+    public void openAddContact(View view) {
+        Intent intention = new Intent(this, AjoutContact.class);
+        startActivity(intention);
     }
 }
