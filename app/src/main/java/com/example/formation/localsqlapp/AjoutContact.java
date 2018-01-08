eef49187f9628f14fce7d2f06bb90e1bd5de0e1b
Formation <toto.titi@free.fr>
2018-01-08 15:58:20 +0100

    Création requete bdd pour insertion contact

--- app/src/main/java/com/example/formation/localsqlapp/AjoutContact.java
+++ app/src/main/java/com/example/formation/localsqlapp/AjoutContact.java
@@ -1,9 +1,16 @@
 package com.example.formation.localsqlapp;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import fr.sm.database.DatabaseHandler;
 
 public class AjoutContact extends AppCompatActivity {
 
@@ -14,5 +21,32 @@ public class AjoutContact extends AppCompatActivity {
     }
     public void valideMail(View v){
         Button clickedButton = (Button) v;
+        long llNum = 0;
+        String lsMessage = "";
+        // récupération des données saisies dans activité ajout_contact
+        EditText edtNom = (EditText) findViewById(R.id.edtNom);
+        String prenom = ((EditText) findViewById(R.id.edtPrenom)).getText().toString();
+        String email = ((EditText) findViewById(R.id.edtMail)).getText().toString();
+
+        // instanciation de la connexion
+        DatabaseHandler db = new DatabaseHandler(this);
+
+        // définition des données à insérer dans la table contact
+        // utilisation d'un hashMap
+        ContentValues hmValeurs = new ContentValues();
+        hmValeurs.put("first_name",edtNom.getText().toString());
+        hmValeurs.put("name", prenom);
+        hmValeurs.put("email",email );
+
+        // --- Insertion
+        try {
+            llNum = db.getWritableDatabase().insert("contact",null,hmValeurs);
+            lsMessage = String.valueOf(llNum) + " enregistrement ajouté";
+        }
+        catch(SQLiteException ex) {
+            Log.e("Sql Exception", ex.getMessage());
+            lsMessage = "Insertion ratée";
+        }
+        Toast.makeText(this,lsMessage, Toast.LENGTH_LONG).show();
     }
 }
